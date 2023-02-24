6dd5f1bc42e5b8c1161e97f21f386c8295959415
Gabriel Fiamete da Rosa <gabriel.fiamete@icloud.com>
2023-02-24 17:08:11 -0300

    Add files via upload

---
+++ jogos.py
@@ -0,0 +1,19 @@
+import forca
+import adivinhacao
+
+print('============================')
+print('=== ESCOLHA SEU JOGO =======')
+print('============================')
+
+print(" (1) Forca (2) Adivinhação")
+
+jogo = input(int('Qual jogo?'))
+
+if(jogo == 1 ):
+    print('Jogando forca')
+    forca.jogar()
+elif( jogo == 2 ):
+    print('Jogando adivinhação')
+    adivinhacao.jogar()
+else:
+    print('Opção inválida')
